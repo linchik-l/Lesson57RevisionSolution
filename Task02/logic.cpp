8958cf55ee79f5fbfed4b2d546075a555ce29cf5
linchic-l <lebedevskayalina@gmail.com>
2025-05-18 18:11:00 +0300

    Solve task02

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -13,5 +13,15 @@
 #include "logic.h"
 
 bool is_power_of_four(int number) {
-	return false;
+	if (number <= 0) {
+		return false;
+	}
+	if (number == 1) {
+		return true;
+	}
+	if (number % 4 != 0 ) {
+		return false;
+	}
+
+	return is_power_of_four(number / 4);
 }
